fcd0488114370a9cc56b980e931d0dc8518de4da
BlacyAnonymous333 <blackyanonymous8@gmail.com>
2024-03-28 00:28:04 +0500

    Creating a CLI number guessing game

---
+++ main.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+//Number guessing game using typescript language...
+import inquirer from "inquirer";
+console.log("\n\t>>> CLI Number Guessing Game <---> Created By \'MALIK SHAHMEER\' <<<\n");
+let GuessNumber = Math.floor(Math.random() * 9 + 1);
+while (true) {
+    let input = await inquirer.prompt({ name: "guessNum", type: "number", message: "What's your guess number? (Between 1 to 9)" });
+    let result = input.guessNum;
+    if (GuessNumber === result) {
+        console.log("Wow !  You guess correct number");
+        break;
+    }
+    else {
+        console.log("Please try again");
+    }
+}
